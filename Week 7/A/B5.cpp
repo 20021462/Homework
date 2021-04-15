d36dd92f3d6a162cd7715a0aa996df4d98893023
Alter <crimson.lotus.201@gmail.com>
2021-04-16 00:43:20 +0700

    Week 9 Homework

---
+++ Week 7/A/B5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+char* weird_string() {
+    char c[] = { 1,2,3,4,5 };
+    return c;
+}
+
+int main()
+{
+    int main(int argc, const char* argv[]) {
+        cout << "Number of command-line arguments: " << argc << endl;
+        for (int i = 0; i < argc; i++) {
+            cout << "Argument #" << i << ": _" << argv[i] << "_\n";
+        }
+        return 0;
+    }
+}
